b37c3a3f99eba211b18db699d08dff86b5440b37
osheen4778 <103919637+osheen4778@users.noreply.github.com>
2023-02-22 11:11:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+$('#navbar').load('navbar.html');
+$('#footer').load('footer.html');
+const API_URL = 'http://localhost:5000/api';
+
+// const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+$.get(`${API_URL}/devices`)
+.then(response => {
+  response.forEach(device => {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
+  });
+})
+.catch(error => {
+  console.error(`Error: ${error}`);
+});
+
+
+
+// devices.forEach(function(device) {
+//   $('#devices tbody').append(
+//     <tr>
+//       <td>${device.user}</td>
+//       <td>${device.name}</td>
+//     </tr>
+//   );
+// });
+
+$('#add-device').on('click', () => {
+  const name = $('#name').val();
+  const user = $('#user').val();
+  const sensorData = [];
+
+  const body = {
+    name,
+    user,
+    sensorData
+  };
+
+  $.post(`${API_URL}/devices`, body)
+  .then(response => {
+    location.href = '/';
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+});
+
+// $.get('http://localhost:3001/devices')
+// .then(response => {
+//   console.log(response);
+// })
+// .catch(error => {
+//   console.log(`Error: ${error}`);
+// });
+
+  
+
+
+  
